bf40d2fb785ec9685f50c75a788d2971e4961e64
Joyce <joyce.zhangg@gmail.com>
2015-12-01 03:04:30 +0000

    Added default mailer url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Nomster::Application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { :host => 'nomster-joz.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
