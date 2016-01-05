49fe6b9538503f4b919d0862f223e9dc1b15c737
Joyce <joyce.zhangg@gmail.com>
2016-01-05 07:15:19 +0000

    Modified tests, added Fancy Box on Place Show page for pictures

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -6,6 +6,11 @@ class CommentsControllerTest < ActionController::TestCase
 	  sign_in user
 	  place = FactoryGirl.create(:place)
 
+    #expect NotificationMailer to be called with method comment_added, this tells it to ignore it (stubbing)
+    #expect return nil, have return be something other than nil as part of stubbing that would accept deliver method
+
+    #can configure domain here directly as well, but this will ACTUALLY SEND AN EMAIL
+
     assert_difference 'Comment.count' do
       post :create, :place_id => place.id, :comment => {
         :message => 'this is a comment',
