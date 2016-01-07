03153f2b4c7dead7b313d75824674ebcc490fbde
Joyce <joyce.zhangg@gmail.com>
2016-01-07 06:20:58 +0000

    Fixed indentation on comment controller test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-	test "create a comment" do
-	  user = FactoryGirl.create(:user)
-	  sign_in user
-	  place = FactoryGirl.create(:place)
+  test "create a comment" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    place = FactoryGirl.create(:place)
 
     #expect NotificationMailer to be called with method comment_added, this tells it to ignore it (stubbing)
     #expect return nil, have return be something other than nil as part of stubbing that would accept deliver method
@@ -16,8 +16,9 @@ class CommentsControllerTest < ActionController::TestCase
         :message => 'this is a comment',
         :rating => 'three stars'      }
     end
+    
     assert_redirected_to place_path(place)
     assert_equal 1, place.comments.count
-	end
+  end
 
 end
